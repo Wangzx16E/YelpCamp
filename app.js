898eeaabf299c516345a6e2a84c5dd31f0d6cd71
Ellen Wang <ellenwang@Ellens-MacBook-Air.local>
2022-01-27 15:40:24 -0800

    home and campground basic page

--- app.js
+++ app.js
@@ -27,12 +27,14 @@ app.get('/', (req, res) => {
 
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', {campgrounds})
+    res.render('campgrounds/index', {campgrounds});
 })
 
-app.get('/campgrounds/:id', async(req, res) => {
-    res.render('campgrounds/show')
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', {campground});
 })
+
 app.listen(3000, ()=> {
     console.log('serving on port 3000')
 })
